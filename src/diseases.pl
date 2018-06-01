d1e41a26d96008272410dc330e4a8f2642947057
Patrick T Cox <pxcox003@odu.edu>
2018-06-01 18:50:05 -0400

    Added diseases dictionary

---
+++ src/diseases.pl
@@ -0,0 +1,467 @@
+% Sypmtoms
+symptom('typhoid', 'muscle pain').
+symptom('typhoid', 'bloating').
+symptom('typhoid', 'constipation').
+symptom('typhoid', 'nausea').
+symptom('typhoid', 'vomiting').
+symptom('typhoid', 'fatigue').
+symptom('typhoid', 'fever').
+symptom('typhoid', 'chills').
+symptom('typhoid', 'loss of appetite').
+symptom('typhoid', 'malaise' ).
+symptom('hepatitis B','tiredness').
+symptom('hepatitis B','fever').
+symptom('hepatitis B','headache').
+symptom('hepatitis B','loss of appetite').
+symptom('hepatitis B','vomiting').
+symptom('hepatitis B','stomach pain').
+symptom('hepatitis B','tan stool' ).
+symptom('hepatitis B','dark urine').
+symptom('hepatitis B','jaundice' ).
+symptom('hsv 1', 'fever').
+symptom('hsv 1', 'swollen lymph nodes').
+symptom('hsv 1', 'tingling').
+symptom('hsv 1', 'sore throat').
+symptom('hsv 1', 'dry cough').
+symptom('hsv 1', 'rash').
+symptom('meningitis', 'headache').
+symptom('meningitis', 'fever').
+symptom('meningitis', 'stiff neck' ).
+symptom('meningitis', 'loss of appetite').
+symptom('meningitis', 'sleepiness').
+symptom('meningitis', 'vomiting').
+symptom('rabies', 'loss of appetite').
+symptom('rabies', 'fever').
+symptom('rabies', 'headache').
+symptom('rabies', 'excessive salivation').
+symptom('rabies', 'muscle spasms').
+symptom('rabies', 'paralysis').
+symptom('rabies', 'confusion').
+symptom('rabies', 'hyperactivity').
+symptom('rabies', 'hallucinations').
+symptom('rabies', 'hydrophobia').
+symptom('diphteria', 'sore throat').
+symptom('diphteria', 'fever').
+symptom('diphteria', 'malaise').
+symptom('diphteria', 'swollen lymph nodes').
+symptom('diphteria', 'nasal discharge').
+symptom('diphteria', 'impaired voice').
+symptom('diphteria', 'rash').
+symptom('cellulitis', 'swollen lymph nodes').
+symptom('cellulitis', 'swelling skin').
+symptom('cellulitis', 'redness of skin').
+symptom('cellulitis', 'fever').
+symptom('mononucleosis', 'swollen lymph nodes').
+symptom('mononucleosis', 'sore throat').
+symptom('mononucleosis', 'headache').
+symptom('mononucleosis', 'abdominal pain').
+symptom('mononucleosis', 'fever').
+symptom('tularemia', 'swollen lymph nodes').
+symptom('tularemia', 'loss of appetite').
+symptom('tularemia', 'fever').
+symptom('tularemia', 'signs of sepsis').
+symptom('tularemia', 'ulcers').
+symptom('chickenpox', 'fever').
+symptom('chickenpox', 'tiredness').
+symptom('chickenpox', 'headache').
+symptom('chickenpox', 'pneumonia').
+symptom('chickenpox', 'inflammation of brain').
+symptom('chickenpox', 'bacterial skin infection').
+symptom('chickenpox', 'rash').
+symptom('measles', 'fever').
+symptom('measles', 'cough').
+symptom('measles', 'runny nose').
+symptom('measles', 'inflammation of eyes').
+symptom('measles', 'facial rash').
+symptom('erythema Infectiosum', 'low grade fever').
+symptom('erythema Infectiosum', 'headache').
+symptom('erythema Infectiosum', 'coryza').
+symptom('erythema Infectiosum', 'bright red rash on cheeks').
+symptom('erythema Infectiosum', 'abdominal pain').
+symptom('erythema Infectiosum', 'pruritus').
+symptom('zika', 'fever').
+symptom('zika', 'rash').
+symptom('zika', 'joint pain').
+symptom('zika', 'headache').
+symptom('zika', 'pink eye').
+symptom('zika', 'muscle pain').
+symptom('african Trypanosomiasis', 'fever').
+symptom('african Trypanosomiasis', 'headache').
+symptom('african Trypanosomiasis', 'joint pain').
+symptom('african Trypanosomiasis', 'itching').
+symptom('african Trypanosomiasis', 'confusion').
+symptom('african Trypanosomiasis', 'tremors').
+symptom('african Trypanosomiasis', 'muscle weakness').
+symptom('african Trypanosomiasis', 'paralysis').
+symptom('pneumonic plague', 'fever').
+symptom('pneumonic plague', 'weakness').
+symptom('pneumonic plague', 'headache').
+symptom('pneumonic plague', 'nausea').
+symptom('pneumonic plague', 'shortness of breath').
+symptom('pneumonic plague', 'chest pain').
+symptom('pneumonic plague', 'cough').
+symptom('lyme Disease', 'fever').
+symptom('lyme Disease', 'headache').
+symptom('lyme Disease', 'rash').
+symptom('lyme Disease', 'muscle pain').
+symptom('lyme Disease', 'joint pain').
+symptom('lyme Disease', 'heart palpitation').
+symptom('lyme Disease', 'numbness in limbs').
+symptom('rocky Mountain Spotted Fever', 'fever').
+symptom('rocky Mountain Spotted Fever', 'headache').
+symptom('rocky Mountain Spotted Fever', 'rash').
+symptom('rocky Mountain Spotted Fever', 'nausea').
+symptom('rocky Mountain Spotted Fever', 'vomiting').
+symptom('rocky Mountain Spotted Fever', 'muscle pain').
+symptom('rocky Mountain Spotted Fever', 'joint pain').
+symptom('rocky Mountain Spotted Fever', 'abdominal pain').
+symptom('rocky Mountain Spotted Fever', 'diarrhea').
+symptom('heartland virus', 'fever').
+symptom('heartland virus', 'headache').
+symptom('heartland virus', 'fatigue').
+symptom('heartland virus', 'nausea').
+symptom('heartland virus', 'diarrhea').
+symptom('heartland virus', 'muscle pain').
+symptom('heartland virus', 'joint pain').
+symptom('heartland virus', 'low white blood cell count').
+symptom('heartland virus', 'low platelet count').
+symptom('severe acute respiratory syndrome', 'fever').
+symptom('severe acute respiratory syndrome', 'myalgia').
+symptom('severe acute respiratory syndrome', 'lethargy').
+symptom('severe acute respiratory syndrome', 'cough').
+symptom('severe acute respiratory syndrome', 'sore throat').
+symptom('severe acute respiratory syndrome', 'non-specific').
+symptom('cholera', 'diarrhea').
+symptom('cholera', 'vomiting').
+symptom('cholera', 'muscle cramps').
+symptom('bolivian hemorrhagic fever', 'fever').
+symptom('bolivian hemorrhagic fever', 'malaise').
+symptom('bolivian hemorrhagic fever', 'headache').
+symptom('bolivian hemorrhagic fever', 'nose bleeds').
+symptom('bolivian hemorrhagic fever', 'blood spots').
+symptom('argentine hemorrhagic fever', 'fever').
+symptom('argentine hemorrhagic fever', 'malaise').
+symptom('argentine hemorrhagic fever', 'headache').
+symptom('argentine hemorrhagic fever', 'dizziness').
+symptom('argentine hemorrhagic fever', 'blood spots').
+symptom('ebola', 'fever').
+symptom('ebola', 'malaise').
+symptom('ebola', 'headache').
+symptom('ebola', 'chills').
+symptom('ebola', 'muscle pain').
+symptom('mumps', 'fever').
+symptom('mumps', 'malaise').
+symptom('mumps', 'headache').
+symptom('mumps', 'parotid gland swelling').
+symptom('yellow fever','fever').
+symptom('yellow fever','headache').
+symptom('yellow fever','chills').
+symptom('yellow fever','back pain').
+symptom('yellow fever','fatigue').
+symptom('yellow fever','loss of appetite').
+symptom('yellow fever','muscle pain').
+symptom('yellow fever','nausea').
+symptom('yellow fever','vomiting').
+symptom('west Nile fever','fever').
+symptom('west Nile fever','headache').
+symptom('west Nile fever','chills').
+symptom('west Nile fever','excessive sweating').
+symptom('west Nile fever','weakness').
+symptom('west Nile fever','fatigue').
+symptom('west Nile fever','swollen lymph nodes').
+symptom('west Nile fever','drowsiness').
+symptom('west Nile fever','joint pain').
+symptom('west Nile fever','nausea').
+symptom('west Nile fever','vomiting').
+symptom('west Nile fever','loss of appetite').
+symptom('west Nile fever','diarrhea').
+symptom('kuru', 'ataxia').
+symptom('kuru', 'dysarthria').
+symptom('kuru', 'tremors').
+symptom('kuru', 'joint pain').
+symptom('kuru', 'dementia').
+symptom('gerstmann Straussler Scheinker syndrome','ataxia').
+symptom('gerstmann Straussler Scheinker syndrome','dementia').
+symptom('gerstmann Straussler Scheinker syndrome','dysarthia').
+symptom('creutzfeldt Jakob disease', 'dementia').
+symptom('creutzfeldt Jakob disease', 'ataxia').
+symptom('creutzfeldt Jakob disease', 'hallucinations').
+symptom('greyscale', 'psychosis').
+symptom('greyscale', 'hallucinations').
+symptom('greyscale', 'mania').
+symptom('greyscale', 'confusion').
+symptom('greyscale', 'calciphylaxis').
+symptom('greyscale', 'skin necrosis').
+symptom('greyscale', 'physical examination').
+symptom('neonatal lupus','skin rash').
+symptom('neonatal lupus','low blood count').
+symptom('psoriasis','skin rash').
+symptom('psoriasis','scaly skin').
+symptom('lichen planus','skin rash').
+symptom('lichen planus','ulcers').
+symptom('leprosy','lesion').
+symptom('leprosy','muscle weakness').
+symptom('leprosy','enlarged nerves').
+symptom('leprosy','eye problems').
+symptom('leprosy','dry skin').
+symptom('filariasis','lymphedema').
+symptom('chikungunya','fever').
+symptom('chikungunya','joint pain').
+symptom('chikungunya','headache').
+symptom('chikungunya','muscle­ pain').
+symptom('chikungunya','rash').
+symptom('chikungunya','joint swelling').
+symptom('hepatitis C','fever').
+symptom('hepatitis C','joint pain').
+symptom('hepatitis C','vomiting').
+symptom('hepatitis C','nausea').
+symptom('hepatitis A','fever').
+symptom('hepatitis A','nausea').
+symptom('hepatitis A','vomiting').
+symptom('hepatitis A','diarrhea').
+symptom('gonorrhea','fever').
+symptom('gonorrhea','sore throat').
+symptom('gonorrhea','vomiting').
+symptom('gonorrhea','abnormal vaginal discharge').
+symptom('aquarium granuloma', 'peripheral papule').
+symptom('aquarium granuloma', 'peripheral vesicle').
+symptom('mycobacterium shottsi', 'peripheral nodules').
+symptom('buruli ulcer', 'papules').
+symptom('buruli ulcer', 'external ulcer').
+symptom('alpha gal', 'mammallian meat allergy').
+symptom('alpha gal', 'rash').
+symptom('alpha gal', 'hives').
+symptom('alpha gal', 'anaphylaxis').
+symptom('valley fever', 'fatigue').
+symptom('valley fever', 'fever').
+symptom('valley fever', 'cough').
+symptom('valley fever', 'rash').
+symptom('valley fever', 'joint pain').
+symptom('valley fever', 'headache').
+
+% Origin
+locale('typhoid', 'Central Asia').
+locale('typhoid', 'East Asia').
+locale('typhoid', 'North America').
+locale('hepatitis A', 'worldwide').
+locale('hepatitis B', 'Africa').
+locale('hepatitis B', 'East Asia').
+locale('hepatitis C', 'worldwide').
+locale('hsv 1', 'worldwide').
+locale('rocky Mountain Spotted Fever', 'North America').
+locale('rocky Mountain Spotted Fever', 'central America').
+locale('rocky Mountain Spotted Fever', 'South America').
+locale('heartland virus', 'North America').
+locale('chickenpox', 'worldwide').
+locale('measles', 'Europe').
+locale('measles', 'Central Asia').
+locale('measles', 'East Asia').
+locale('measles', 'North America').
+locale('measles', 'Africa').
+locale('erythema Infectiosum', 'North America').
+locale('erythema Infectiosum', 'Europe').
+locale('bolivian hemorrhagic fever', 'South America'). 
+locale('argentine hemorrhagic fever', 'South America'). 
+locale('ebola', 'Africa').
+locale('greyscale', 'Westeros').
+locale('zika','Africa').
+locale('zika','South America').
+locale('african Trypanosomiasis', 'Africa').
+locale('pneumonic plague', ' ').
+locale('valley fever', 'North America').
+locale('aquarium granuloma', ' ').
+locale('mycobacterium shottsi', 'North America').
+locale('buruli ulcer', 'Africa').
+locale('alpha gal', 'North America').
+locale('meningitis', 'worldwide').
+
+% Treatment
+treatment('hepatitis C', 'anitiviral drug').
+treatment('hepatitis C', 'liver transplant').
+treatment('hepatitis A', 'rest').
+treatment('hepatitis A', 'rehydration').
+treatment('gonorrhea', 'antibiotics').
+treatment('hepatitis B', 'no treatment').
+treatment('typhoid','antibiotics').
+treatment('hsv 1', 'antiviral').
+treatment('meningitis', 'Antibiotics').
+treatment('meningitis', 'Steroid').
+treatment('meningitis', 'Penicillin').
+treatment('meningitis', 'Hospitalization').
+treatment('meningitis', 'Oxygen therapy').
+treatment('rabies', 'blood transfusion').
+treatment('rabies', 'antiviral').
+treatment('diphteria', 'antibiotics').
+treatment('diphteria', 'penicillin').
+treatment('diphteria', 'antitoxin').
+treatment('cellulitis', 'antibiotics').
+treatment('cellulitis', 'surgical drainage of pus').
+treatment('mononucleosis', 'paracetamol').
+treatment('mononucleosis', 'nsaids').
+treatment('tularemia', 'doxycycline').
+treatment('tularemia', 'gentamicin').
+treatment('tularemia', 'streptomycin').
+treatment('tularemia', 'ciprofloxacin').
+treatment('chickenpox', 'antihistamines').
+treatment('chickenpox', 'topical ointments').
+treatment('measles', 'no treatment').
+treatment('measles', 'drinking fluids').
+treatment('measles', 'rest').
+treatment('erythema Infectiosum', 'acetaminophen').
+treatment('erythema Infectiosum', 'rest').
+treatment('erythema Infectiosum', 'drinking fluids').
+treatment('zika','rest').
+treatment('african Trypanosomiasis', 'intravenous pentamidine').
+treatment('african Trypanosomiasis', 'intramuscular pentamidine').
+treatment('african Trypanosomiasis', 'nifurtimox').
+treatment('african Trypanosomiasis', 'eflornithine').
+treatment('pneumonic plague', 'streptomycin').
+treatment('pneumonic plague', 'gentamicin').
+treatment('pneumonic plague', 'tetracyclines').
+treatment('pneumonic plague', 'chloramphenicol ').
+treatment('lyme Disease', 'amoxicillin').
+treatment('lyme Disease', 'tetracycline').
+treatment('rocky Mountain Spotted Fever', 'tetracycline').
+treatment('rocky Mountain Spotted Fever', 'chloramphenicol').
+treatment('heartland virus', 'no treatment').
+treatment('severe acute respiratory syndrome', 'antipyretics').
+treatment('severe acute respiratory syndrome', 'mechanical ventilation').
+treatment('cholera', 'oral rehydration therapy').
+treatment('cholera', 'zinc supplementation').
+treatment('cholera', 'antibiotics').
+treatment('cholera', 'intravenous fluids').
+treatment('bolivian hemorrhagic fever', 'vesper mice reduced contact ').
+treatment('bolivian hemorrhagic fever', 'junin vaccination').
+treatment('argentine hemorrhagic fever', 'junin vaccination').
+treatment('ebola', 'blood transfusion').
+treatment('ebola', 'iv fluids').
+treatment('ebola', 'oxygen therapy').
+treatment('mumps','icing neck').
+treatment('mumps','heating neck').
+treatment('mumps','acetaminophin').
+treatment('yellow fever','hospitalization').
+treatment('west Nile fever','hospitalization').
+treatment('west Nile fever','intravenous fluids').
+treatment('west Nile fever','respiratory support').
+treatment('kuru', 'symptom management').
+treatment('gerstmann Straussler Scheinker syndrome','symptom management').
+treatment('creutzfeldt Jakob disease','symptom management').
+treatment('greyscale', 'prayer').
+treatment('greyscale', 'sacrifice').
+treatment('greyscale', 'fasting').
+treatment('neonatal lupus','topical steroids').
+treatment('psoriasis','corticosteroids').
+treatment('lichen planus','corticosteroids'). 
+treatment('chikungunya','no treatment').
+treatment('filariasis','antibiotics').
+treatment('filariasis','ivermectin').
+treatment('filariasis','albendazole').
+treatment('leprosy','antibiotics').
+treatment('aquarium granuloma', 'antimycobacterials').
+treatment('aquarium granuloma', 'hot compresses').
+treatment('aquarium granuloma', 'surgical removal').
+treatment('mycobacterium shottsi', 'antimycobacterials').
+treatment('mycobacterium shottsi', 'hot compresses').
+treatment('buruli ulcer', 'antimycobacterials').
+treatment('buruli ulcer', 'hot compresses').
+treatment('buruli ulcer', 'surgical removal').
+treatment('alpha gal', 'become vegetarian').
+treatment('valley fever', 'antifungal medication'). 
+
+% Test
+test('hepatitis B', 'blood test').
+test('hsv 1', 'blood test').
+test('hsv 1', 'physical examination').
+test('meningitis', 'blood cultures').
+test('meningitis', 'computerized tomography').
+test('meningitis', 'lumbar puncture').
+test('rabies', 'siliva').
+test('rabies', 'lumbar puncture').
+test('rabies', 'skin biopsy').
+test('diphteria', 'Toxigenicity').
+test('diphteria', 'polymerase chain reaction(PCR)').
+test('diphteria', 'serologic testing').
+test('cellulitis', 'complete blood count(CBC)').
+test('cellulitis', 'blood culture').
+test('mononucleosis', 'mononuclear spot test').
+test('mononucleosis', 'ebv antibody test').
+test('tularemia', 'blood test for f. tularensis').
+test('tularemia', 'sputum culture').
+test('chickenpox', 'varicella Test').
+test('chickenpox', 'enzyme-linked immunosorbent assay (ELISA)').
+test('chickenpox', 'physical examination').
+test('measles', 'rubeola antibody blood test').
+test('measles', 'Immunity Test').
+test('measles', 'physical examination').
+test('erythema Infectiosum', 'parvovirus B19 blood test').
+test('erythema Infectiosum', 'physical examination').
+test('african Trypanosomiasis', 'trypanosomes in blood').
+test('zika', 'trioplex rrt-pcr').
+test('zika', 'mac-enzyme-linked immunosorbent assay (ELISA)').
+test('pneumonic plague', 'blood test').
+test('pneumonic plague', 'lymph nodes').
+test('pneumonic plague', 'lungs').
+test('lyme Disease', 'enzyme-linked immunosorbent assay (ELISA)').
+test('lyme Disease', 'western blot').
+test('rocky Mountain Spotted Fever', 'Indirect immunofluorescence assay (IFA)').
+test('rocky Mountain Spotted Fever', 'serology').
+test('rocky Mountain Spotted Fever', 'skin biopsy').
+test('heartland virus', 'no test').
+test('severe acute respiratory syndrome', 'enzyme-linked immunosorbent assay (ELISA)').
+test('severe acute respiratory syndrome', 'immunofluorescence').
+test('severe acute respiratory syndrome', 'pcr').
+test('severe acute respiratory syndrome', 'chest x-ray').
+test('cholera', 'stool test').
+test('bolivian hemorrhagic fever', 'enzyme-linked immunosorbent assay (ELISA)').
+test('bolivian hemorrhagic fever', 'igm antibodies test').
+test('bolivian hemorrhagic fever', 'igg antibodies test').
+test('argentine hemorrhagic fever', 'rtpcr').
+test('argentine hemorrhagic fever', 'enzyme-linked immunosorbent assay (ELISA)').
+test('ebola', 'blood test').
+test('ebola', 'polymerase chain reaction (PCR)').
+test('ebola', 'enzyme-linked immunosorbent assay (ELISA)').
+test('mumps','enzyme-linked immunosorbent assay (ELISA)').
+test('mumps','Indirect immunofluorescence assay (IFA)').
+test('mumps','igm antibodies test').
+test('yellow fever','enzyme-linked immunosorbent assay (ELISA)').
+test('yellow fever','igm antibodies test').
+test('yellow fever','igg antibodies test').
+test('yellow fever','microsphere-based immunoassay (MIA)').
+test('west Nile fever','physical examination').
+test('west Nile fever','spinal tap').
+test('west Nile fever','electroencephalograph (EEG)').
+test('west Nile fever','mri').
+test('west Nile fever','cerebrospinal fluid (CSF) analysis').
+test('west Nile fever','igm antibodies test').
+test('west Nile fever','igg antibodies test').
+test('kuru', 'neurological exam').
+test('kuru', 'blood test').
+test('gerstmann Straussler Scheinker syndrome','genetic testing').
+test('gerstmann Straussler Scheinker syndrome','nervous system test').
+test('creutzfeldt Jakob disease','electroencephalogram').
+test('creutzfeldt Jakob disease','spinal fluid test').
+test('greyscale', 'skin biopsy').
+test('typhoid', 'blood').
+test('chikungunya','enzyme-linked immunosorbent assay (ELISA)').
+test('filariasis','serology').
+test('filariasis','microscopic testing').
+test('leprosy','skin biopsy').
+test('leprosy','nerve biopsy').
+test('hepatitis C','HCV RNA tests').
+test('hepatitis A',' HAV igm antibodies test antibody test').
+test('gonorrhea','Nucleic Acid Amplification').
+test('gonorrhea','Urine sample').
+test('buruli ulcer', 'gram stain').
+test('buruli ulcer', 'photochromogenicity').
+test('mycobacterium shottsi', 'gram stain').
+test('mycobacterium shottsi', 'gene sequence').
+test('mycobacterium shottsi', 'photochromogenicity').
+test('aquarium granuloma', 'gram stain').
+test('aquarium granuloma', 'photochromogenicity').
+test('alpha gal', 'blood test').
+test('valley fever', 'blood test').
+test('valley fever', 'chest xray').
+test('valley fever', 'lung ct scan').
+test('neonatal lupus', 'no test').
+test('psoriasis', 'physical exam').
+test('lichen planus', 'physical exam').
